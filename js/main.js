7227e8ab453e60f551a68197a811a0c3124b0915
Varun Agrawal <varagrawal@gmail.com>
2019-07-02 01:27:15 -0400

    print bye for extra participant when number is odd

--- js/main.js
+++ js/main.js
@@ -1,53 +1,68 @@
-function get_schedule(n, x, round) {
+function get_schedule(x, round) {
     var schedule = [[1, x[x.length - 1]]];
     for (var i = 0; i < x.length / 2 - 1; i++) {
         schedule.push([x[i], x[x.length - 2 - i]]);
     }
     return schedule;
 }
-function get_round_output(text, round, schedule) {
+function get_round_output(text, round, schedule, num_participants, odd) {
     text += "<h5> Round " + (round + 1) + " </h5>";
     text += "<ul class=\"list-group\">";
+    var first, second;
     for (var _i = 0, schedule_1 = schedule; _i < schedule_1.length; _i++) {
         var x = schedule_1[_i];
-        text = text + ("<li class=\"list-group-item\">" + x[0] + " - " + x[1] + "</li>");
+        first = x[0], second = x[1];
+        if (odd) {
+            if (first === num_participants) {
+                first = "bye";
+            }
+            else if (second === num_participants) {
+                second = "bye";
+            }
+        }
+        text = text + ("<li class=\"list-group-item\">" + first + " - " + second + "</li>");
     }
     text += "</ul>";
     text += "<br/><br/>";
     return text;
 }
-function print_schedule(x, num_rounds) {
+function print_schedule(x, num_participants, odd) {
     var schedule;
     var text = "";
     var output = document.getElementById("output");
-    for (var round = 0; round < num_rounds - 1; round++) {
-        schedule = get_schedule(num_rounds, x, round);
-        text = get_round_output(text, round, schedule);
+    var num_rounds = num_participants - 1;
+    for (var round = 0; round < num_rounds; round++) {
+        schedule = get_schedule(x, round);
+        text = get_round_output(text, round, schedule, num_participants, odd);
         console.log("Round: " + round);
         x.unshift(x.pop());
     }
     output.innerHTML = text;
 }
 function run_algorithm() {
-    var num_rounds = +document.getElementById("num_rounds").value;
-    if (num_rounds % 2 == 1) {
-        num_rounds += 1;
+    var num_participants = +document.getElementById("num_participants").value;
+    var odd = false;
+    if (num_participants % 2 == 1) {
+        num_participants += 1;
+        odd = true;
     }
-    console.log(num_rounds);
+    console.log("Number of participants: " + num_participants);
     var x = [];
-    for (var i = 2; i <= num_rounds; i++) {
+    for (var i = 2; i <= num_participants; i++) {
         x.push(i);
     }
-    print_schedule(x, num_rounds);
+    print_schedule(x, num_participants, odd);
 }
-document.getElementById("go").addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("go").click();
-    }
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("go").addEventListener("keyup", function (event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById("go").click();
+        }
+    });
 });
 function check_input() {
-    var input_box = document.getElementById("num_rounds");
+    var input_box = document.getElementById("num_participants");
     var num_rounds = Number(input_box.value);
     var go_button = document.getElementById("go");
     if (isNaN(num_rounds) || num_rounds <= 0) {
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-function get_schedule(n: number, x: number[], round: number) {
+function get_schedule(x: number[], round: number) {
     let schedule: number[][] = [[1, x[x.length - 1]]];
 
     for (let i = 0; i < x.length / 2 - 1; i++) {
@@ -8,11 +8,25 @@ function get_schedule(n: number, x: number[], round: number) {
     return schedule;
 }
 
-function get_round_output(text: string, round: number, schedule: number[][]) {
+function get_round_output(text: string, round: number,
+    schedule: number[][], num_participants: number, odd: boolean) {
     text += `<h5> Round ${round + 1} </h5>`;
     text += `<ul class="list-group">`;
+
+    let first, second;
+
     for (let x of schedule) {
-        text = text + `<li class="list-group-item">${x[0]} - ${x[1]}</li>`;
+        [first, second] = x;
+        if (odd) {
+            if (first === num_participants) {
+                first = "bye";
+            } else if (second === num_participants) {
+                second = "bye";
+            }
+        }
+
+        // console.log(first, second);
+        text = text + `<li class="list-group-item">${first} - ${second}</li>`;
         // console.log(x);
     }
     text += `</ul>`;
@@ -21,17 +35,19 @@ function get_round_output(text: string, round: number, schedule: number[][]) {
     return text;
 }
 
-function print_schedule(x: number[], num_rounds: number) {
+function print_schedule(x: number[], num_participants: number, odd: boolean) {
     let schedule: number[][];
     let text: string = "";
     let output: HTMLParagraphElement = document.getElementById("output") as HTMLParagraphElement;
 
-    for (let round = 0; round < num_rounds - 1; round++) {
-        schedule = get_schedule(num_rounds, x, round);
+    let num_rounds = num_participants - 1;
+    for (let round = 0; round < num_rounds; round++) {
+        schedule = get_schedule(x, round);
 
-        text = get_round_output(text, round, schedule);
+        text = get_round_output(text, round, schedule, num_participants, odd);
         console.log(`Round: ${round}`);
 
+        // rotate the participants
         x.unshift(x.pop());
     }
 
@@ -39,25 +55,29 @@ function print_schedule(x: number[], num_rounds: number) {
 }
 
 function run_algorithm() {
-    let num_rounds: number = +(document.getElementById("num_rounds") as HTMLInputElement).value;
+    let num_participants: number = +(document.getElementById("num_participants") as HTMLInputElement).value;
 
-    if (num_rounds % 2 == 1) {
-        num_rounds += 1;
+    let odd: boolean = false;
+    if (num_participants % 2 == 1) {
+        num_participants += 1;
+        odd = true;
     }
 
-    console.log(num_rounds);
+    console.log(`Number of participants: ${num_participants}`);
 
     let x: number[] = []
-    for (var i = 2; i <= num_rounds; i++) {
+    for (var i = 2; i <= num_participants; i++) {
         x.push(i)
     }
 
-    print_schedule(x, num_rounds);
+    print_schedule(x, num_participants, odd);
 }
 
-document.getElementById("go").addEventListener("keyup", (event) => {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        (document.getElementById("go") as HTMLButtonElement).click();
-    }
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("go").addEventListener("keyup", (event) => {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            (document.getElementById("go") as HTMLButtonElement).click();
+        }
+    });
 });
